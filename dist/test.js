4875102e12813ec3f87c96c9a13bbda798cf5490
Rizzzi Git <rizzzigit@gmail.com>
2022-07-04 16:15:13 +0800

    rework

--- dist/test.js
+++ dist/test.js
@@ -5,11 +5,11 @@ var net_1 = tslib_1.__importDefault(require("net"));
 var crypto_1 = tslib_1.__importDefault(require("crypto"));
 var _1 = tslib_1.__importDefault(require("."));
 var time = function () { return Math.round(Date.now() / 1000); };
-var key = '0'.repeat(64);
+var key = Buffer.from('0'.repeat(64), 'hex');
 var run = function () { return tslib_1.__awaiter(void 0, void 0, void 0, function () {
-    var map, server, client, connection, _a, _b;
-    return tslib_1.__generator(this, function (_c) {
-        switch (_c.label) {
+    var map, server, client, connection, timeout, count;
+    return tslib_1.__generator(this, function (_a) {
+        switch (_a.label) {
             case 0:
                 map = {
                     echo: function (_, arg0) { return arg0; },
@@ -20,8 +20,8 @@ var run = function () { return tslib_1.__awaiter(void 0, void 0, void 0, functio
                 server = new _1.default.Server(net_1.default.createServer(), map, { key: key });
                 return [4 /*yield*/, server.listen(Number(process.argv[2]))];
             case 1:
-                _c.sent();
-                return [3 /*break*/, 5];
+                _a.sent();
+                return [3 /*break*/, 8];
             case 2:
                 client = new _1.default.Agent(map, { key: key });
                 return [4 /*yield*/, client.connect({
@@ -29,14 +29,25 @@ var run = function () { return tslib_1.__awaiter(void 0, void 0, void 0, functio
                         port: Number(process.argv[3])
                     })];
             case 3:
-                connection = _c.sent();
-                console.log(connection);
-                _b = (_a = console).log;
-                return [4 /*yield*/, connection.exec('echo', 'Hello')];
+                connection = _a.sent();
+                timeout = Date.now() + 10000;
+                count = 0;
+                _a.label = 4;
             case 4:
-                _b.apply(_a, [_c.sent()]);
-                _c.label = 5;
-            case 5: return [2 /*return*/];
+                if (!true) return [3 /*break*/, 6];
+                console.log(++count);
+                return [4 /*yield*/, connection.exec('read', 16 * 1024)];
+            case 5:
+                _a.sent();
+                if (timeout < Date.now()) {
+                    return [3 /*break*/, 6];
+                }
+                return [3 /*break*/, 4];
+            case 6: return [4 /*yield*/, connection.destroy()];
+            case 7:
+                _a.sent();
+                _a.label = 8;
+            case 8: return [2 /*return*/];
         }
     });
 }); };
